04088a956b2677f580e4b8109c6dd9cf5a807dcc
yym1928 <yimingyu919@gmail.com>
2021-09-09 14:53:53 -0400

    updatd

--- Single/gameState.js
+++ Single/gameState.js
@@ -1,7 +1,19 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
 class GameState {
-    constructor(boolean, score1, score2) {
-      this.boolean = boolean;
-      this.score1 = score1;
-      this.score2 = score2;
-    }
+
+  constructor(boolean, score1, score2) {
+    this.boolean = boolean;
+    this.score1 = score1;
+    this.score2 = score2;
   }
+}
+
+class Player {
+
+}
+
+class Ball {
+
+}
